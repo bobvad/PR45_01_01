642a287bdd0b0bb8a4eeaa4e9c4a757f21514346
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-24 09:24:11 +0500

    Разработанная структура проекта.Дегтянников

---
+++ API_Degtiynnikov/Startup.cs
@@ -0,0 +1,54 @@
+﻿namespace API_Degtiynnikov
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(); 
+            services.AddSwaggerGen(); 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseCors("AllowAll"); // Использование CORS
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // Для Web API
+            });
+        }
+    }
+}
+
